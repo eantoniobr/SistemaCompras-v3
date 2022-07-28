1679a9b4bbf2cf525660104e811866d72b9b6c73
Eric Antonio da Silva <eantonio_br@hotmail.com>
2022-07-28 17:54:54 -0300

    Primeiro envio do projeto Sistema de Compras.

---
+++ SistemaCompra.Infra.Data/SistemaCompraContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SistemaCompra.Domain.Core;
+using SistemaCompra.Infra.Data.Produto;
+using ProdutoAgg = SistemaCompra.Domain.ProdutoAggregate;
+using SolicitacaoCompraAgg = SistemaCompra.Domain.SolicitacaoCompraAggregate;
+using SolicitacaoAgg = SistemaCompra.Domain.SolicitacaoCompraAggregate;
+
+namespace SistemaCompra.Infra.Data
+{
+    public class SistemaCompraContext : DbContext
+    {
+        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        public SistemaCompraContext(DbContextOptions options) : base(options) { }
+        public DbSet<ProdutoAgg.Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProdutoAgg.Produto>()
+                .HasData(
+                    new ProdutoAgg.Produto("Produto01", "Descricao01", "Madeira", 100)
+                );
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+
+
+            modelBuilder.Entity<SolicitacaoCompraAgg.SolicitacaoCompra>()
+                .HasData(
+                    new SolicitacaoCompraAgg.SolicitacaoCompra(usuarioSolicitante: "Solicitante01", nomeFornecedor: "Fornecedor01")
+                );
+
+            modelBuilder.ApplyConfiguration(new SolicitacaoCompraConfiguration());
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(loggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=SistemaCompraDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //.UseSqlServer(@"Server=localhost\SQLEXPRESS01;Database=SistemaCompraDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
